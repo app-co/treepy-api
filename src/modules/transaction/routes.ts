ba1fec049a4e93fea8e3a020a22b810b6e8c868d
app-co <william@appcom.dev>
2025-03-14 07:42:47 -0300

    .

--- src/modules/transaction/routes.ts
+++ src/modules/transaction/routes.ts
@@ -1,12 +1,13 @@
-import { Auth } from '@/shared/middlewares/verify-jwt';
-import { FastifyInstance } from 'fastify';
-import { ControlerTransactions } from './controler';
+import { Auth } from "@/shared/middlewares/verify-jwt";
+import type { FastifyInstance } from "fastify";
+import { ControlerTransactions } from "./controler";
 
-const controler = new ControlerTransactions()
+const controler = new ControlerTransactions();
 
 export async function routeTransaction(app: FastifyInstance) {
-  app.addHook('onRequest', Auth)
+	app.addHook("onRequest", Auth);
 
-  app.post('/transaction-card', controler.card)
-  app.post('/transaction-pix', controler.pix)
+	app.post("/transaction-card", controler.card);
+	app.post("/transaction-pix", controler.pix);
+	app.post("/transaction-boleto", controler.boleto);
 }
